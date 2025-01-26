e1682dd22c8714424708d7a45c1848906f9ed756
Sriyantha Senarathna <senalmdv@gmail.com>
2025-01-26 19:08:20 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,22 +1,40 @@
 const mega = require("megajs");
+
+// Authentication details
 const auth = {
     email: 'sanarathnas0@gmail.com',
     password: 'Sena11@#usn',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
-}
+};
 
+/**
+ * Uploads a file to MEGA.
+ * @param {ReadableStream} data - The readable stream of the file to upload.
+ * @param {string} name - The name of the file to be uploaded.
+ * @returns {Promise<string>} - Resolves with the public URL of the uploaded file.
+ */
 const upload = (data, name) => {
     return new Promise((resolve, reject) => {
         try {
             const storage = new mega.Storage(auth, () => {
-                data.pipe(storage.upload({name: name, allowUploadBuffering: true}));
+                const uploadStream = storage.upload({ name, allowUploadBuffering: true });
+                data.pipe(uploadStream);
+
                 storage.on("add", (file) => {
                     file.link((err, url) => {
-                        if (err) throw err;
-                        storage.close()
-                        resolve(url);
+                        if (err) {
+                            reject(err);
+                        } else {
+                            resolve(url);
+                        }
+                        storage.close();
                     });
                 });
+            });
+
+            storage.on("error", (err) => {
+                reject(err); // Catch storage-related errors
+                storage.close();
             });
         } catch (err) {
             reject(err);
